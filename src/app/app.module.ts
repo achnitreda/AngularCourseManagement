b75b2c0f2c55cfd4f4c05328b3a280fec4306f1d
Achnit Reda <achnitreda@gmail.com>
2022-07-06 14:51:58 +0100

    start with NgRx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,12 @@ import { CourseComponent } from './course/course.component';
 import { ProfessorComponent } from './professor/professor.component';
 import { StudentComponent } from './student/student.component';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { environment } from 'src/environments/environment';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { courseReducer } from './state/course/course.reducres';
+import { EffectsModule } from '@ngrx/effects';
+import { CourseEffects } from './state/course/course.effets';
 
 @NgModule({
   declarations: [
@@ -19,6 +25,13 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    StoreModule.forRoot({
+      course : courseReducer
+    }),
+    EffectsModule.forRoot([
+      CourseEffects
+    ]),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
   ],
   providers: [],
   bootstrap: [AppComponent]
